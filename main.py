7f92e7e1946a6dc4cf7b6136f303b9a38d2c552e
Daniels1610 <danielagraz1610@hotmail.com>
2023-05-18 16:47:37 -0700

    Add DFA model Class with conversionToCFG

---
+++ main.py
@@ -0,0 +1,24 @@
+from models.DFA import DFA
+
+def getInputDFA():
+    file = open('assets/input.txt', 'r')
+    definitionDFA = []
+    for line in file.readlines():
+        definitionDFA.append(list(line.strip().split(',')))
+    return definitionDFA
+
+if __name__ == "__main__":
+    inputDFA = getInputDFA()
+    myDFA = DFA(
+        states=inputDFA[0],
+        alphabet=inputDFA[1],
+        transitionStates=inputDFA[4:],
+        startState=inputDFA[2],
+        acceptState=inputDFA[3]
+    )
+    
+    print(myDFA.getDefinitionDFA())
+    print(myDFA.conversionToCFG(myDFA.transitionStates))
+
+
+    
---
+++ models/DFA.py
@@ -0,0 +1,33 @@
+class DFA:
+    states = []
+    alphabet = []
+    transitionStates = []
+    startState = ""
+    acceptState = []
+    
+    def __init__(self, states:list, alphabet:list, transitionStates:list, startState:str, acceptState:list):
+        self.states = states
+        self.alphabet = alphabet
+        self.transitionStates = transitionStates
+        self.startState = startState
+        self.acceptState = acceptState
+
+    def getDefinitionDFA(self):
+        return {
+            "states" : self.states,
+            "alphabet" : self.alphabet,
+            "transitionStates" : self.transitionStates,
+            "startState" : self.startState,
+            "acceptState" : self.acceptState
+        }
+
+    def conversionToCFG(self, transitionStates:list):
+        rules = {}
+        for transition in transitionStates:
+            rules[transition[0]] = []
+
+        for transition in transitionStates:
+            rules[transition[0]].append(f"{transition[1]}{transition[2]}") 
+            
+        return rules
+                
