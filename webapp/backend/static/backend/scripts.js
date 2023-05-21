60535d69a1302e48e38fa5e8b5e6e4c6765e100c
Leobardo Luis Argüelles <66451459+LeobardoArguelles@users.noreply.github.com>
2023-05-21 13:55:18 -0700

    Add input for transition function

--- webapp/backend/static/backend/scripts.js
+++ webapp/backend/static/backend/scripts.js
@@ -28,6 +28,7 @@ function changeStates(statesDiv, quantity) {
         // Add classes to newDiv
         newDiv.classList.add("flex", "flex-row", "items-center", "justify-center", "space-x-6");
         let newP = document.createElement("p");
+        newP.className = "state-name";
         // Add the state name to the p tag using the value of statesValue to select
         // a letter from the alphabet using ascii code
         newP.innerHTML = String.fromCharCode(64 + statesValue);
@@ -84,6 +85,85 @@ function changeStates(statesDiv, quantity) {
     }
 }
 
+function clearTransitions() {
+    let transitions = document.getElementById("transitions");
+    transitions.innerHTML = "";
+}
+
+// Function that triggers when the button with id "process" is clicked.
+// It creates a div for every state (which are the divs with class "state-name")
+// and adds them to the div with id "transitions". Each of these divs have an input
+// for every terminal symbol, which are read from the input with id "terminals".
+function processInput() {
+    clearTransitions();
+    // Get all states
+    let states = document.getElementsByClassName("state-name");
+    let terminals = document.getElementById("terminals").value;
+    let transitions = document.getElementById("transitions");
+
+    // Delete blank spaces from terminals
+    terminals = terminals.replace(/\s/g, "");
+    // Split terminals into an array
+    terminals = terminals.split(",");
+
+    // Iterate over every state
+    // For every state, create a div with class "transition-div" and add it to the
+    // transitions div
+    for (let i = 0; i < states.length; i++) {
+        let state = states[i];
+        let transitionDiv = document.createElement("div");
+        transitionDiv.className = "transition-div";
+        transitionDiv.classList.add("flex", "flex-col", "items-center", "justify-center", "space-y-2");
+
+        // Add a header with the state name to the transition div
+        let header = document.createElement("h2");
+        header.innerHTML = state.innerHTML;
+        transitionDiv.appendChild(header);
+
+        // For each terminal symbol, create div with class "transition-input-div"
+        // and add it to the transition div.
+        // This div contains a label with the terminal symbol and a select with
+        // the states as options.
+        // The select has a name that is the state name + the terminal symbol.
+        // The label has a for attribute that is the same as the select name.
+        // The select has an id that is the same as the select name.
+        for (let j = 0; j < terminals.length; j++) {
+            let terminal = terminals[j];
+            let transitionInputDiv = document.createElement("div");
+            transitionInputDiv.className = "transition-input-div";
+            transitionInputDiv.classList.add("flex", "flex-row", "items-center", "justify-center", "space-x-2");
+
+            let label = document.createElement("label");
+            label.innerHTML = terminal;
+            label.htmlFor = state.innerHTML + terminal;
+
+            let select = document.createElement("select");
+            select.name = state.innerHTML + terminal;
+            select.id = state.innerHTML + terminal;
+
+            // Add an option for every state
+            for (let k = 0; k < states.length; k++) {
+                let option = document.createElement("option");
+                option.value = states[k].innerHTML;
+                option.innerHTML = states[k].innerHTML;
+                select.appendChild(option);
+            }
+
+            transitionInputDiv.appendChild(label);
+            transitionInputDiv.appendChild(select);
+            transitionDiv.appendChild(transitionInputDiv);
+        }
+        transitions.appendChild(transitionDiv);
+
+        // Add a divider between every transition div
+        if (i < states.length - 1) {
+            let divider = document.createElement("hr");
+            divider.className = "transition-divider";
+            transitions.appendChild(divider);
+        }
+    }
+}
+
 
 // On page load, call anonymous function that calls changeStates
 window.onload = function() {
@@ -91,6 +171,7 @@ window.onload = function() {
     let minus = document.getElementById("minus");
     let plus = document.getElementById("plus");
     let statesDiv = document.getElementById("states-div");
+    let process = document.getElementById("process");
 
 
     minus.addEventListener("click", function() {
@@ -108,4 +189,6 @@ window.onload = function() {
             changeStates(statesDiv, 1);
         }
     });
+
+    process.addEventListener("click", processInput);
 }
